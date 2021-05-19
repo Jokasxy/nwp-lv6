870eef92b150c0509fd700324c241fd7e493a3ee
Jokasxy <josip_1997@live.com>
2021-05-19 23:24:12 +0200

    Projects route added with CRUD

--- model/project.js
+++ model/project.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var projectsSchema = new mongoose.Schema({  
   name: String,
   description: String,
+  price: { type: Number, min: 0 },
   done: String,
   start: { type: Date, default: Date.now },
   end: { type: Date, default: Date.now }
