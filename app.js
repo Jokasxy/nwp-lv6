24dfd45c427558490331169a7f7d2e5b21a48220
Jokasxy <josip_1997@live.com>
2021-05-19 23:47:48 +0200

    Projects route added in app js

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var projectsRouter = require('./routes/projects');
+
+var db = require('./model/db'),
+    blob = require('./model/projects');
+
 
 var app = express();
 
@@ -20,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/projects', projectsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
